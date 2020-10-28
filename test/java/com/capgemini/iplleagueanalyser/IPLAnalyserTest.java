b813951891833bfd01027275b5c22dcc64407724
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-28 13:05:40 +0530

    Added: Top Bowling Rate for Enum And Method

--- test/java/com/capgemini/iplleagueanalyser/IPLAnalyserTest.java
+++ test/java/com/capgemini/iplleagueanalyser/IPLAnalyserTest.java
@@ -27,59 +27,68 @@ public class IPLAnalyserTest {
 		iplAnalyser.loadBattingData(BATTING_DATA_PATH);
 		iplAnalyser.loadBowlingData(BOWLING_DATA_PATH);
 	}
-	
+
 	@Test
-    public void givenBattingDataCSVFile_ShouldLoadBattingData() throws IPLAnalyserException{
-        int totalRecords = iplAnalyser.loadBattingData(BATTING_DATA_PATH);
-        assertEquals(100, totalRecords);
-    }
-	
+	public void givenBattingDataCSVFile_ShouldLoadBattingData() throws IPLAnalyserException {
+		int totalRecords = iplAnalyser.loadBattingData(BATTING_DATA_PATH);
+		assertEquals(100, totalRecords);
+	}
+
 	@Test
 	public void givenBowlingDataCSVFile_ShouldLoadBowlingData() throws IPLAnalyserException {
 		int totalRecords = iplAnalyser.loadBowlingData(BOWLING_DATA_PATH);
 		assertEquals(99, totalRecords);
 	}
-	
+
 	@Test
 	public void givenBattingData_WhenSortedByAvg_ShouldReturnHighestAvgFirst() throws IPLAnalyserException {
-		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.BAT_AVG,"Batsman");
+		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.BAT_AVG, "Batsman");
 		assertEquals("83.2", sortedBattingList.get(0).getAvg());
 	}
-	
+
 	@Test
 	public void givenBattingData_WhenSortedBySR_ShouldReturnHighestSRFirst() throws IPLAnalyserException {
-		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.BAT_SR,"Batsman");
+		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.BAT_SR, "Batsman");
 		assertEquals("333.33", sortedBattingList.get(0).getStrikeRate());
 	}
-	
+
 	@Test
-	public void givenBattingData_WhenSortedByBoundaries_ShouldReturnHighestTotalBoundaries() throws IPLAnalyserException {
-		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOUNDARIES,"Batsman");
-		int totalBoundaries = Integer.parseInt(sortedBattingList.get(0).getFours())+Integer.parseInt(sortedBattingList.get(0).getSixes());
+	public void givenBattingData_WhenSortedByBoundaries_ShouldReturnHighestTotalBoundaries()
+			throws IPLAnalyserException {
+		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOUNDARIES, "Batsman");
+		int totalBoundaries = Integer.parseInt(sortedBattingList.get(0).getFours())
+				+ Integer.parseInt(sortedBattingList.get(0).getSixes());
 		assertEquals(83, totalBoundaries);
 	}
-	
+
 	@Test
-	public void givenBattingData_WhenSortedByStrikeRateAndBoundaries_ShouldReturnSortedList() throws IPLAnalyserException {
-		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.SR_AND_BOUNDARIES,"Batsman");
+	public void givenBattingData_WhenSortedByStrikeRateAndBoundaries_ShouldReturnSortedList()
+			throws IPLAnalyserException {
+		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.SR_AND_BOUNDARIES, "Batsman");
 		assertEquals("Ishant Sharma", sortedBattingList.get(0).getPlayer());
 	}
-	
+
 	@Test
 	public void givenBattingData_WhenSortedByAvgAndStrikeRate_ShouldReturnSortedList() throws IPLAnalyserException {
-		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.AVG_AND_SR,"Batsman");
+		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.AVG_AND_SR, "Batsman");
 		assertEquals("MS Dhoni", sortedBattingList.get(0).getPlayer());
 	}
-	
+
 	@Test
 	public void givenBattingData_WhenSortedByRunsAndAvg_ShouldReturnSortedList() throws IPLAnalyserException {
-		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.RUNS_AND_AVG,"Batsman");
+		sortedBattingList = iplAnalyser.getSortedList(FlexibleSort.Order.RUNS_AND_AVG, "Batsman");
 		assertEquals("David Warner", sortedBattingList.get(0).getPlayer());
 	}
-	
+
 	@Test
 	public void givenBowlingData_WhenSortedByAvg_ShouldReturnBestAvgFirst() throws IPLAnalyserException {
-		sortedBowlingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOWL_AVG,"Bowler");
+		sortedBowlingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOWL_AVG, "Bowler");
 		assertEquals("11", sortedBowlingList.get(0).getAvg());
+	}
+
+	@Test
+	public void givenBowlingData_WhenSortedByStrikeRate_ShouldReturnBestStrikeRateFirst() throws IPLAnalyserException {
+		sortedBowlingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOWL_SR, "Bowler");
+		assertEquals("8.66", sortedBowlingList.get(0).getStrikeRate());
 	}
 }

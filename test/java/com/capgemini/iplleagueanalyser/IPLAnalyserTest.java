e30403c0cb8bbe75c77736173869d99b93d630dc
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-28 13:35:36 +0530

    Added: Best Bowling Average And Strike Rate To Enum And Methods Tests

--- test/java/com/capgemini/iplleagueanalyser/IPLAnalyserTest.java
+++ test/java/com/capgemini/iplleagueanalyser/IPLAnalyserTest.java
@@ -105,10 +105,18 @@ public class IPLAnalyserTest {
 		sortedBowlingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOWL_SR_AND_ECON, "Bowler");
 		assertEquals("8.66", sortedBowlingList.get(0).getStrikeRate());
 	}
+
 	@Test
 	public void givenBowlingData_WhenSortedBySRandWicketHauls_ShouldReturnBestSortedList() throws IPLAnalyserException {
 		iplAnalyser.loadBowlingData(BOWLING_DATA_PATH);
-		sortedBowlingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOWL_SR_AND_WICKET_HAULS,"Bowler");
+		sortedBowlingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOWL_SR_AND_WICKET_HAULS, "Bowler");
 		assertEquals("16.81", sortedBowlingList.get(0).getStrikeRate());
+	}
+
+	@Test
+	public void givenBowlingData_WhenSortedByAvgandSr_ShouldReturnBestSortedList() throws IPLAnalyserException {
+		iplAnalyser.loadBowlingData(BOWLING_DATA_PATH);
+		sortedBowlingList = iplAnalyser.getSortedList(FlexibleSort.Order.BOWL_AVG_AND_SR, "Bowler");
+		assertEquals("11", sortedBowlingList.get(0).getAvg());
 	}
 }
